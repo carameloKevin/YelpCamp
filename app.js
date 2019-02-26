24d47c1c54a8b203019c3fbfa71461670105373d
Kevin Berg <bergkevin1996@gmail.com>
2019-02-26 20:54:19 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    Campground      = require("./models/campground"),
+    flash           = require("connect-flash"),
+    seedDB          = require("./seeds"),
+    Comment         = require("./models/comment"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    User            = require("./models/user"),
+    methodOverride  = require("method-override");
+    
+// seed the database 
+// seedDB();
+
+//requiring routes
+var commentsRoutes       = require("./routes/comments"),
+    campgroundsRoutes   = require("./routes/campgrounds"),
+    indexRoutes         = require("./routes/index");
+
+
+mongoose.connect("mongodb://localhost/yelp_campV2", { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname +"/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser  = req.user;
+    res.locals.error        = req.flash("error");
+    res.locals.success      = req.flash("success")
+    next();
+})
+
+app.use("/campgrounds/:id/comments", commentsRoutes);
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundsRoutes);
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The YelpCamp Server has started!");
+});
