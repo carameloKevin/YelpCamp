94fb577cc80023bade990f6ac549e602e57d9836
Kevin Berg <bergkevin1996@gmail.com>
2019-03-01 02:08:04 +0000

    fix mongodb with atlas. Change password

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ var commentsRoutes       = require("./routes/comments"),
     campgroundsRoutes   = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
 
-
-mongoose.connect("mongodb+srv://caramelokevin:xrhJW;eyG+ejzE9si7v^-.@cluster0-ecarf.mongodb.net/test?retryWrites=true");
+mongoose.connect("mongodb+srv://caramelokevin:nq9olA46DYU928D@cluster0-ecarf.mongodb.net/test?retryWrites=true");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname +"/public"));
